57fa0e73419a0de0822850ea98c9c177bc5df2e3
Sean Kidwell <dev.seankidwell@gmail.com>
2018-04-28 19:07:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(arr, ele) {
-  arr = [ele, ...arr];
-  return arr;
+function addElementToBeginningOfArray(array, element) {
+  newArray = [element, ...array];
+  return newArray;
 }
 function destructivelyAddElementToBeginningOfArray(arr, ele) {
   
