9118d5efbbaa76a02c49d130a64803c2cef627ce
Sean Kidwell <dev.seankidwell@gmail.com>
2018-04-28 19:16:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,11 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
